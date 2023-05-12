44db1cb04f6a86e30e1650bda674f9ef1d0cd635
isadora-ribeiro <isadorasantosribeiro4@gmail.com>
2023-05-12 18:00:40 -0300

    arquivos c#

---
+++ bytebank_ADM/SistemaInterno/Autenticavel.cs
@@ -0,0 +1,19 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.SistemaInterno
+{
+    public interface Autenticavel
+    {
+        public string Senha { get; set; }
+
+        public abstract bool Autenticar(string senha);
+        
+   
+    }
+}
---
+++ bytebank_ADM/SistemaInterno/SistemaInterno.cs
@@ -0,0 +1,46 @@
+﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Parceria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.SistemaInterno
+{
+    public class SistemaInterno
+    {
+        public bool Logar(Autenticavel funcionario, string senha)
+        {
+            bool usuarioAutenticardo = funcionario.Autenticar(senha);
+            if (usuarioAutenticardo)
+            {
+                Console.WriteLine("Boas vindas ao nosso sistema. ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta!");
+                return false;
+            }
+        }
+
+        public bool Logar(GerenteDeContas funcionario, string senha)
+        {
+            bool usuarioAutenticardo = funcionario.Autenticar(senha);
+            if (usuarioAutenticardo)
+            {
+                Console.WriteLine("Boas vindas ao nosso sistema. ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta!");
+                return false;
+            }
+        }
+
+      
+        }
+    }
+
